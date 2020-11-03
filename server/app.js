ed384c43bb17640b286cf3244f06e4e2eb67767a
Chris Baird <chrisbaird05@icloud.com>
2020-11-03 10:58:09 -0800

    Populated api app file
    populated api app file with preset boilerplate

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { join } = require("path");
+const routes = require("./routes");
+const app = express();
+
+// Loading dotenv file
+require("dotenv").config();
+
+// Logger middleware
+morgan(":method :url :status :res[content-length] - :response-time ms");
+
+// Mongoose connection string
+mongoose.connect("mongodb://127.0.0.1:27017/passport-jwt");
+// Mongoose error handler
+mongoose.connection.on("error", (error) => console.log(error));
+// Mongoose promise configuration
+mongoose.Promise = global.Promise;
+
+// Loading auth middleware
+require("./auth/auth");
+
+// Loading bodyparser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+console.log(__dirname);
+// Serves up react app
+app.use(express.static(join(__dirname, "/client/build")));
+
+// Routes configuration
+app.use(routes);
+
+//Handle errors
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+module.exports = app;
