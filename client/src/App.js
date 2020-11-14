b35524e005c36b3c4940dfad097f829897afbbbf
Chris baird <chrisbaird05@icloud.com>
2020-11-14 12:26:27 -0800

    Landing section
    Completed landing section

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./componets/Navbar";
+import Landing from "./sections/Landing";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Landing />
     </div>
   );
 }
