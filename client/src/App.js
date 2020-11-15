35366241208d805eda4d4c1a1dd6132be9b853e0
Chris baird <chrisbaird05@icloud.com>
2020-11-14 20:03:44 -0800

    Commitment section
    Created my commitment section with placeholder information.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Projects from "./sections/Projects";
 import About from "./sections/About";
 import Technologies from "./sections/Technologies";
 import Work from "./sections/Work";
+import Commitment from "./sections/Commitment";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       <Projects />
       <Technologies />
       <Work />
+      <Commitment />
     </div>
   );
 }
---
+++ client/src/sections/Commitment.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+export default function Commitment() {
+  return (
+    <section className="bg-light pt-5">
+      <Container>
+        <h2 className="text-center">My Developer Commitment</h2>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in
+          augue eget ipsum imperdiet condimentum. Quisque consequat id quam sit
+          amet fermentum. Mauris mattis lacinia nibh in vestibulum. In placerat
+          pretium mattis. Integer vel sapien eu neque pretium tempus. Nunc vitae
+          sapien justo. Donec vitae nisl accumsan, lobortis felis vitae, porta
+          risus. Donec pellentesque, dui ac sodales mollis, est ante cursus ex,
+          et fringilla felis purus sit amet ipsum.
+        </p>
+        <span>Chris Baird</span>
+      </Container>
+    </section>
+  );
+}
