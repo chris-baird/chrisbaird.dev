1952efaac041281ae8f4914fb9dfba6dac0fa34c
Chris baird <chrisbaird05@icloud.com>
2020-11-14 17:35:42 -0800

    Landing background
    Implemented custom background for landing section.

--- client/src/sections/Landing.js
+++ client/src/sections/Landing.js
@@ -2,20 +2,36 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 export default function Landing() {
   return (
-    <Container className="mt-5 mb-5 pt-5 pb-5">
-      <Row>
-        <Col xs="12" sm="12" md="8" lg="8" xl="8">
-          <h1 className="font-weight-bolder">Chris Baird</h1>
-          <h2 className="mt-2 mb-2">Full Stack Web Developer</h2>
-          <p className="lead font-weight-light">
-            Motivated developer with experience creating custom web applications
-            through Javascript and React. Strong collaboration skills and proven
-            history of application development. Methodical approach to solving
-            problems. Proficient in learning new technologies and implementing
-            solutions.
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <section
+      style={{
+        backgroundImage: "url(./landing-bg.png)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Container className="mb-5 pt-5 pb-5">
+        <Row>
+          <Col
+            className="text-warning"
+            xs="12"
+            sm="12"
+            md={{ size: 8, offset: 5 }}
+            lg="8"
+            xl="8"
+          >
+            <h1 className="font-weight-bolder">Chris Baird</h1>
+            <h2 className="mt-2 mb-2">Full Stack Web Developer</h2>
+            <p className="lead font-weight-light">
+              Motivated developer with experience creating custom web
+              applications through Javascript and React. Strong collaboration
+              skills and proven history of application development. Methodical
+              approach to solving problems. Proficient in learning new
+              technologies and implementing solutions.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 }
