ca55cfb9bf02b224e330783d6a4edc9796f6e215
Chris baird <chrisbaird05@icloud.com>
2020-11-15 22:09:40 -0800

    Technology react map
    Set up basic map to test technologies coming from api.

--- client/src/componets/TechnologyCard.js
+++ client/src/componets/TechnologyCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Col } from "reactstrap";
-export default function TechnologyCard() {
+export default function TechnologyCard(props) {
   return (
     <Col>
-      <img src="https://via.placeholder.com/50x50" alt="Icon" />
-      <p>Name</p>
+      <img src={props.imageUrl} alt="Icon" />
+      <p>{props.technologyName}</p>
     </Col>
   );
 }
--- client/src/sections/Technologies.js
+++ client/src/sections/Technologies.js
@@ -1,44 +1,38 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import TechnologyCard from "../componets/TechnologyCard";
 import { Container, Row } from "reactstrap";
-import {getTechnologies} from '../helpers/api'
+import { getTechnologies } from "../helpers/api";
 
 export default function Technologies() {
   const [technologies, setTechnologies] = useState([]);
 
-  useEffect(async() => {
+  useEffect(async () => {
     console.log(technologies);
-   if(technologies.length === 0) {
-     console.log("inside if");
-     try {
-      const apiTechnolgies = await getTechnologies();
-      console.log(apiTechnolgies)
+    if (technologies.length === 0) {
+      console.log("inside if");
+      try {
+        const apiTechnolgies = await getTechnologies();
+        console.log(apiTechnolgies);
 
-      setTechnologies(apiTechnolgies)
-
-     } catch (error) {
-       console.log(error)
-     }
-   }
-  },[])
+        setTechnologies(apiTechnolgies);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, []);
 
   return (
     <section className="bg-light pb-5">
       <Container>
         <h2 className="mt-5 mb-5 pt-5 text-center">Technologies I Know</h2>
         <Row>
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
-          <TechnologyCard />
+          {technologies.map((technology) => (
+            <TechnologyCard
+              key={technology._id}
+              imageUrl={technology.imageUrl}
+              technologyName={technology.technologyName}
+            />
+          ))}
         </Row>
       </Container>
     </section>
