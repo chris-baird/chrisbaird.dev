28ed43c602e9bc7152f42c1fc16078a2c57f4c85
Chris baird <chrisbaird05@icloud.com>
2020-11-15 22:02:40 -0800

    Technology api post route & controller
    Created route and contoller file to test creating a technology with postman.

--- server/controllers/technologyController.js
+++ server/controllers/technologyController.js
@@ -1,11 +1,19 @@
-const TechnologyModel = require('../models/technology')
+const TechnologyModel = require("../models/technology");
 module.exports = {
-    getTechnologies: async (req, res) => {
-        try {
-            const dbTechnologies = await TechnologyModel.find()
-            res.json(dbTechnologies)
-        } catch (error) {
-            res.json(error)
-        }
+  getTechnologies: async (req, res) => {
+    try {
+      const dbTechnologies = await TechnologyModel.find();
+      res.json(dbTechnologies);
+    } catch (error) {
+      res.json(error);
     }
-}
+  },
+  createTechnology: async (req, res) => {
+    try {
+      const newDbTechnology = await TechnologyModel.create(req.body);
+      res.json(newDbTechnology);
+    } catch (error) {
+      res.json(error);
+    }
+  },
+};
--- server/routes/api-routes/technologyRoutes.js
+++ server/routes/api-routes/technologyRoutes.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
-const technologyController = require('../../controllers/technologyController')
-router.use('/', technologyController.getTechnologies)
+const router = require("express").Router();
+const technologyController = require("../../controllers/technologyController");
+router.get("/", technologyController.getTechnologies);
+router.post("/", technologyController.createTechnology);
 module.exports = router;
