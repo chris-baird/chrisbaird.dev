2ccb1f20398b57cf99b34679f3778f9be7d490f0
Chris baird <chrisbaird05@icloud.com>
2020-11-19 16:44:35 -0800

    File upload route W/ Test log

--- server/controllers/technologyController.js
+++ server/controllers/technologyController.js
@@ -10,6 +10,9 @@ module.exports = {
   },
   createTechnology: async (req, res) => {
     try {
+      // Test log start
+      console.log(req.files);
+      // End
       const newDbTechnology = await TechnologyModel.create(req.body);
       res.json(newDbTechnology);
     } catch (error) {
--- server/routes/api-routes/technologyRoutes.js
+++ server/routes/api-routes/technologyRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const technologyController = require("../../controllers/technologyController");
+const uploadHandler = require("../../middleware/multer-google-storage");
 router.get("/", technologyController.getTechnologies);
-router.post("/", technologyController.createTechnology);
+router.post("/", uploadHandler.any(), technologyController.createTechnology);
 module.exports = router;
