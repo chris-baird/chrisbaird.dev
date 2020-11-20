874d65144d82f2b87b249e116745ccd0e12f3682
Chris baird <chrisbaird05@icloud.com>
2020-11-19 17:29:29 -0800

    Technology modal
    Set up a reactstrap modal to create a new technology with a form component.

--- client/src/sections/Technologies.js
+++ client/src/sections/Technologies.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 import TechnologyCard from "../componets/TechnologyCard";
-import { Container, Row } from "reactstrap";
+import {
+  Button,
+  Container,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  Row,
+} from "reactstrap";
 import { getTechnologies } from "../helpers/api";
 
 export default function Technologies() {
   const [technologies, setTechnologies] = useState([]);
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
 
   useEffect(() => {
     async function getTechnologyData() {
@@ -26,6 +38,31 @@ export default function Technologies() {
     <section className="bg-light pb-5">
       <Container>
         <h2 className="mt-5 mb-5 pt-5 text-center">Technologies I Know</h2>
+        <div>
+          <Button color="danger" onClick={toggle}>
+            Test
+          </Button>
+          <Modal isOpen={modal} toggle={toggle} className="">
+            <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={toggle}>
+                Do Something
+              </Button>{" "}
+              <Button color="secondary" onClick={toggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </div>
         <Row>
           {technologies.map((technology) => (
             <TechnologyCard
