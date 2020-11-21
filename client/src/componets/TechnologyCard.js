948c444c136d88a61907b20831bed294fb48e607
Chris baird <chrisbaird05@icloud.com>
2020-11-20 17:40:19 -0800

    Reduced size

--- client/src/componets/TechnologyCard.js
+++ client/src/componets/TechnologyCard.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Col } from "reactstrap";
 export default function TechnologyCard(props) {
   return (
-    <Col>
-      <img src={props.imageUrl} alt="Icon" />
+    <Col xs="12" sm="12" md="4" lg="4" xl="4">
+      <img src={props.imageUrl} alt="Icon" style={{ width: "50px" }} />
       <p>{props.technologyName}</p>
     </Col>
   );
