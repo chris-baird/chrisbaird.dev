7ed58b5ed7a929d2637ff1fdf5b1076029a5944b
Chris <chris@Chriss-MacBook-Air.local>
2021-01-04 23:15:33 -0800

    Middleware Test
    Testing middleware functionalty

---
+++ server/__tests__/auth.spec.js
@@ -0,0 +1,16 @@
+const isAuthenticated = require('../middleware/isAuthenticated');
+describe('Auth Middleware Tests', () => {
+    it('calls next function if there is a user', () => {
+        // Test user
+        let user = {
+            email: '',
+            password: ''
+        }
+        // Jest mock for next middleware function
+        let next = jest.fn()
+        // Calling middleware with user and mock response and next callback
+        isAuthenticated({ user }, { json: jest.fn() }, next)
+        // Next function is called if a user is present
+        expect(next).toHaveBeenCalled();
+    })
+})
