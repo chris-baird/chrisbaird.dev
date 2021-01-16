ee858cece5d6f8461b9426ada06342d05f0448e4
Chris baird <chrisbaird05@icloud.com>
2021-01-15 19:25:21 -0800

    hotfix

--- server/app.js
+++ server/app.js
@@ -13,9 +13,6 @@ const app = express()
 // Loading dotenv file
 import "dotenv/config.js"
 
-// Writes buckey key file from env
-generateBucketKey()
-
 // Pulling in routes
 import routes from "./routes/index.js"
 
