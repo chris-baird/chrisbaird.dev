f8c4fbc98c3441eb0ef5cf44216d340b1886c3f2
Chris <cab3953@gmail.com>
2021-01-29 11:26:18 -0800

    New Component.
    Component to hold developer info lists.

---
+++ client/src/sections/DeveloperInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from "reactstrap";
+export default function DeveloperInfo() {
+    return (
+        <section className="pt-5 pb-5">
+            <Container>
+
+            </Container>
+        </section>
+    )
+}
