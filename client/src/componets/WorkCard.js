ee8447995fe4a68307e756539ab4dbf4d6e4ab66
Chris Baird <cab3953@gmail.com>
2021-12-21 12:05:25 -0800

    Cleaned up this component

--- client/src/componets/WorkCard.js
+++ client/src/componets/WorkCard.js
@@ -6,20 +6,27 @@ export default function WorkCard({ imageUrl, companyName, position, points }) {
     <Col xs="12" sm="12" md="12" lg="12" xl="12">
       <section className="bg-light p-2 shadow d-flex m-2">
         <Row>
-      <Col xs="12" sm="12" md="2" lg="2" xl="2" className="d-flex align-items-center">
-        <img src={imageUrl} alt="Work" className="img-fluid" />
-      </Col>
-      <Col xs="12" sm="12" md="10" lg="10" xl="10">
-      <ul className="list-group list-group-flush">
-        <p className="font-weight-bold">{position}</p>
-          {points.map((point) => (
-            <li className="list-group-item" key={point}>{point}</li>
-          ))}
-        </ul>
-      </Col>
-      </Row>  
-        
-        
+          <Col
+            xs="12"
+            sm="12"
+            md="2"
+            lg="2"
+            xl="2"
+            className="d-flex align-items-center"
+          >
+            <img src={imageUrl} alt="Work" className="w-100" />
+          </Col>
+          <Col xs="12" sm="12" md="10" lg="10" xl="10">
+            <ul>
+              <u>
+                <p className="font-weight-bold">{position}</p>
+              </u>
+              {points.map((point) => (
+                <li key={point}>{point}</li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
       </section>
     </Col>
   );
