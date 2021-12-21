a662382864bdd4e63083eb400be96b78ebb3f44a
Chris Baird <chris-baird@users.noreply.github.com>
2021-12-20 20:54:36 -0800

    Update WorkCard.js

--- client/src/componets/WorkCard.js
+++ client/src/componets/WorkCard.js
@@ -3,10 +3,9 @@ import { Col } from "reactstrap";
 
 export default function WorkCard({ imageUrl, companyName, position, points }) {
   return (
-    <Col xs="12" sm="12" md="6" lg="4" xl="4">
-      <section className="bg-light p-2 shadow">
+    <Col xs="12" sm="12" md="12" lg="12" xl="12">
+      <section className="bg-light p-2 shadow d-flex">
         <img src={imageUrl} alt="Work" className="img-fluid" />
-        <h3>{companyName}</h3>
         <p className="text-muted">{position}</p>
         <ul>
           {points.map((point) => (
