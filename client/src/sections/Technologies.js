616484aeac978727b6d869de4a390aad0faec651
Chris Baird <chris-baird@users.noreply.github.com>
2022-01-15 22:59:45 -0800

    Update Technologies.js
    Updated section verbage

--- client/src/sections/Technologies.js
+++ client/src/sections/Technologies.js
@@ -44,7 +44,7 @@ export default function Technologies() {
   return (
     <section className="bg-light pb-5">
       <Container>
-        <h2 className="mt-5 mb-5 pt-5 text-center">Technologies I Know</h2>
+        <h2 className="mt-5 mb-5 pt-5 text-center">Technologies I Have Worked With</h2>
         <div>
           {user.email ? (
             <Button
