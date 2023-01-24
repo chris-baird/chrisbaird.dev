82873214ad800145b487e77a78ef7c50f80773c2
Chris Baird <cab3953@gmail.com>
2023-01-24 13:08:50 -0800

    Added function to wake apps

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
-import Navbar from "./componets/Navbar";
-import Landing from "./sections/Landing";
-import Projects from "./sections/Projects";
-import About from "./sections/About";
-import Technologies from "./sections/Technologies";
-import Work from "./sections/Work";
-import Commitment from "./sections/Commitment";
-import Contact from "./sections/Contact";
-import Footer from "./sections/Footer";
-import UserContext from "./contexts/UserContext";
+import { useState } from "react"
+import Navbar from "./componets/Navbar"
+import Landing from "./sections/Landing"
+import Projects from "./sections/Projects"
+import About from "./sections/About"
+import Technologies from "./sections/Technologies"
+import Work from "./sections/Work"
+import Commitment from "./sections/Commitment"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
+import UserContext from "./contexts/UserContext"
 import DeveloperInfo from "./sections/DeveloperInfo"
+
+function wakeApps() {
+  fetch("https://nano-url.onrender.com")
+  fetch("https://squad-up.onrender.com")
+}
+
 function App() {
-  const [user, setUser] = useState({ email: null, token: null });
+  const [user, setUser] = useState({ email: null, token: null })
+  wakeApps()
 
   // const user = { email: null, token: null };
   return (
@@ -27,7 +34,7 @@ function App() {
         <Footer />
       </UserContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
