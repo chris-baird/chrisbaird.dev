67f3e53b9497ec91cca60d89088581abade7baf4
Chris Baird <cab3953@gmail.com>
2023-05-29 09:58:02 -0700

    Loging feature
    Added a logging feature that writes the request method and time to the logs file

--- server/app.js
+++ server/app.js
@@ -1,48 +1,52 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import morgan from "morgan"
-import { join } from "path"
-import { dirname } from "path"
-import { fileURLToPath } from "url"
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import { join } from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import logger from "./middleware/logger.js";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express()
+const app = express();
 
 // Pulling in routes
-import routes from "./routes/index.js"
+import routes from "./routes/index.js";
 
 // Logger middleware
-morgan(":method :url :status :res[content-length] - :response-time ms")
+morgan(":method :url :status :res[content-length] - :response-time ms");
 
 // Mongoose connection string
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/passport-jwt"
-)
+);
 // Mongoose error handler
 mongoose.connection.on("error", (error) => {
-  throw error
-})
+  throw error;
+});
 // Mongoose promise configuration
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
 // Loading auth middleware
-import "./auth/auth.js"
+import "./auth/auth.js";
 
 // Loading bodyparser middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // Serves up react app
-app.use(express.static(join(__dirname, "../client/build")))
+app.use(express.static(join(__dirname, "../client/build")));
+
+// Logging middleware
+app.use(logger);
 
 // Routes configuration
-app.use(routes)
+app.use(routes);
 
 //Handle errors
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500)
-  res.json({ error: err })
-})
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
 
-export default app
+export default app;
